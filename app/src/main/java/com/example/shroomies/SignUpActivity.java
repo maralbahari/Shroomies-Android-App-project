4d2062b35c0a502d4a657260370ac1fd20781cde
Maral Bahari <Maralbahari75@yahoo.com>
2020-11-20 18:14:39 +0330

    This is the main structure

--- app/src/main/java/com/example/shroomies/SignUpActivity.java
+++ app/src/main/java/com/example/shroomies/SignUpActivity.java
@@ -3,6 +3,8 @@ package com.example.shroomies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -10,5 +12,8 @@ public class SignUpActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+
     }
 }
