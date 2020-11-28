dad27062716d17bb3dd1889125e7fbf48c3f83b2
Aruba <aruba.sid@gmail.com>
2020-11-28 19:52:09 +0530

    Added Login backend

--- app/src/main/java/com/example/shroomies/LoginActivity.java
+++ app/src/main/java/com/example/shroomies/LoginActivity.java
@@ -3,19 +3,33 @@ package com.example.shroomies;
 import android.content.Intent;
 import android.os.Bundle;
 
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     EditText username;
     EditText password;
     Button login;
     TextView signup;
+
+    FirebaseAuth mAuth;
+
     SessionManager sessionManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,12 +38,8 @@ public class LoginActivity extends AppCompatActivity {
         password=findViewById(R.id.password_login);
         login=findViewById(R.id.login_button);
         signup=findViewById(R.id.sign_up_button);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,6 +50,40 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+             String uname_txt = username.getText().toString();
+             String pw_txt = password.getText().toString();
+
+             if (TextUtils.isEmpty(uname_txt) || TextUtils.isEmpty(pw_txt)){
+                 Toast.makeText(LoginActivity.this, "Please fill in your details",Toast.LENGTH_SHORT).show();
+             }
+             else {
+                loginUser(uname_txt, pw_txt);
+             }
+            }
+        });
 
     }
-}
+    private void loginUser(String username, String password){
+        mAuth.signInWithEmailAndPassword(username, password). addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "Welcome to Shroomies!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        }
+    }
