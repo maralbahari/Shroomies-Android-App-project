0246ab5ef2dc2283c6f5ecb39e387ca37b4a5f1e
Aruba <aruba.sid@gmail.com>
2020-11-30 00:53:28 +0530

    Added Google Sign In

--- app/src/main/java/com/example/shroomies/LoginActivity.java
+++ app/src/main/java/com/example/shroomies/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,22 +18,34 @@ import androidx.biometric.BiometricPrompt;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
 import com.google.firebase.auth.FirebaseAuthInvalidUserException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.core.Tag;
 
 import java.io.File;
 import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
-   protected EditText username;
-   protected EditText password;
-    Button login;
+    protected EditText username;
+    protected EditText password;
+    Button login, google_signup;
     TextView signup;
+
+    private final int RC_SIGN_IN = 7;
+    GoogleSignInClient mGoogleSignInClient;
 
     FirebaseAuth mAuth;
 
@@ -43,6 +56,7 @@ public class LoginActivity extends AppCompatActivity {
     private BiometricPrompt biometricPrompt;
     private BiometricPrompt.PromptInfo promptInfo;
     boolean alredyLoggedin = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,50 +74,63 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 }
             }
-        }catch (Exception e){
+        } catch (Exception e) {
         }
-        if(alredyLoggedin){
+        if (alredyLoggedin) {
             startActivity(new Intent(this, MainActivity.class));
             finish();
-            Toast.makeText(LoginActivity.this, "logged in as "+user, Toast.LENGTH_LONG).show();
+            Toast.makeText(LoginActivity.this, "logged in as " + user, Toast.LENGTH_LONG).show();
         }
+
         setContentView(R.layout.activity_login);
-        username=findViewById(R.id.email_login);
-        password=findViewById(R.id.password_login);
-        login=findViewById(R.id.login_button);
-        signup=findViewById(R.id.sign_up_button);
+        username = findViewById(R.id.email_login);
+        password = findViewById(R.id.password_login);
+        login = findViewById(R.id.login_button);
+        signup = findViewById(R.id.sign_up_button);
+        google_signup = findViewById(R.id.google_sign_up);
         mAuth = FirebaseAuth.getInstance();
 
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplication(),SignUpActivity.class);
+                Intent intent = new Intent(getApplication(), SignUpActivity.class);
                 startActivity(intent);
+            }
+        });
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        google_signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
             }
         });
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-             String uname_txt = username.getText().toString();
-             String pw_txt = password.getText().toString();
+                String uname_txt = username.getText().toString();
+                String pw_txt = password.getText().toString();
 
-             if (TextUtils.isEmpty(uname_txt) || TextUtils.isEmpty(pw_txt)){
-                 Toast.makeText(LoginActivity.this, "Please fill in your details",Toast.LENGTH_SHORT).show();
-             }
-             else {
-                loginUser(uname_txt, pw_txt);
-             }
+                if (TextUtils.isEmpty(uname_txt) || TextUtils.isEmpty(pw_txt)) {
+                    Toast.makeText(LoginActivity.this, "Please fill in your details", Toast.LENGTH_SHORT).show();
+                } else {
+                    loginUser(uname_txt, pw_txt);
+                }
             }
         });
 
     }
-    private void loginUser(String username, String password){
-        mAuth.signInWithEmailAndPassword(username, password). addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+    private void loginUser(String username, String password) {
+        mAuth.signInWithEmailAndPassword(username, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
                     Toast.makeText(LoginActivity.this, "Welcome to Shroomies!", Toast.LENGTH_SHORT).show();
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -132,8 +159,9 @@ public class LoginActivity extends AppCompatActivity {
                 }
             }
         });
-        }
-    boolean setBiometric(){
+    }
+
+    boolean setBiometric() {
         executor = ContextCompat.getMainExecutor(this);
         biometricPrompt = new BiometricPrompt(LoginActivity.this,
                 executor, new BiometricPrompt.AuthenticationCallback() {
@@ -141,28 +169,30 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthenticationError(int errorCode,
                                               @NonNull CharSequence errString) {
                 super.onAuthenticationError(errorCode, errString);
-                if (errorCode == BiometricConstants.ERROR_USER_CANCELED){
+                if (errorCode == BiometricConstants.ERROR_USER_CANCELED) {
                     login.setOnTouchListener(null);
-                }else{
+                } else {
                     Toast.makeText(getApplicationContext(),
                             "Authentication error: " + errString, Toast.LENGTH_SHORT)
                             .show();
                 }
                 successBiometric = false;
             }
+
             @Override
             public void onAuthenticationSucceeded(
                     @NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
-                successBiometric=true;
+                successBiometric = true;
             }
+
             @Override
             public void onAuthenticationFailed() {
                 super.onAuthenticationFailed();
                 Toast.makeText(getApplicationContext(), "Authentication failed",
                         Toast.LENGTH_SHORT)
                         .show();
-                successBiometric=false;
+                successBiometric = false;
             }
         });
 
@@ -171,5 +201,55 @@ public class LoginActivity extends AppCompatActivity {
         // if needed by your app.
         return successBiometric;
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                Toast.makeText(LoginActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("Login","firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+
+        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Log.d("Login", "signInWithCredential:Success");
+                    FirebaseUser user = mAuth.getCurrentUser();
+
+                    Toast.makeText(LoginActivity.this, "User Signed In", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Log.w("Login", "signInWithCredentail:Failure", task.getException());
+                    Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+}
+
+
+
+
+
+
+
 
