be66bdc8bd1b4de43592649d8c89099ddda4bab1
Badr-Ba-Sowid <61441185+Badr-Ba-Sowid@users.noreply.github.com>
2021-01-05 23:23:50 +0300

    private and group chat update

--- app/src/main/java/com/example/shroomies/notifications/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/shroomies/notifications/MyFirebaseInstanceIDService.java
@@ -6,6 +6,11 @@ import android.os.IBinder;
 
 import androidx.annotation.Nullable;
 
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdReceiver;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessaging;
+
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
 
 
