08d6cc0cc157223e7a1c47cc64f50caff7d537b0
Maral <62244933+maralbahari@users.noreply.github.com>
2021-07-20 13:33:47 +0800

    missing classes added

---
+++ app/src/main/java/com/example/shroomies/Config.java
@@ -0,0 +1,22 @@
+package com.example.shroomies;
+
+public class Config {
+    public static final String apartmentID="apartmentID";
+    public static final String adminID="admin";
+    public static final String taskCards="taskCard";
+    public static final String expensesCards="expensesCards";
+    public static final String apartmentMembers="apartmentMembers";
+    public static final String logs="logs";
+    public static final String FUNCTION_ADD_EXPENSES_CARDS = "addExpensesCard";
+    public static final String FUNCTION_ADD_TASK_CARDS = "addTasksCards";
+    public static final String FUNCTION_GET_APARTMENT_DETAILS = "getApartmentDetails";
+    public static final String FUNCTION_GET_USER_DETAIL = "getUserDetails";
+    public static final String FUNCTION_LEAVE_APARTMENT = "leaveApartment";
+    public static final String FUNCTION_DELETE_EXPENSE_CARD = "deleteExpensesCard";
+    public static final String FUNCTION_DELETE_TASK_CARD = "deleteTasksCard";
+    public static final String FUNCTION_ARCHIVE_TASKS_CARD = "archiveTasksCard";
+    public static final String FUNCTION_ARCHIVE_EXPENSES_CARD = "archiveExpensesCard";
+
+
+
+}
