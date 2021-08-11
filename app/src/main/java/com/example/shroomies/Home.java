b42534ee62ce044be105d75c6ad013aee77e29df
Maral <62244933+maralbahari@users.noreply.github.com>
2021-08-12 02:57:13 +0800

    home fixed

--- app/src/main/java/com/example/shroomies/Home.java
+++ app/src/main/java/com/example/shroomies/Home.java
@@ -1,32 +1,21 @@
 package com.example.shroomies;
 
 import android.app.Application;
-import android.app.ListActivity;
 import android.content.Intent;
-import android.content.SearchRecentSuggestionsProvider;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import org.jetbrains.annotations.NotNull;
-
-import java.util.HashMap;
 
 public class Home extends Application {
-    SessionManager sessionManager;
     FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener authStateListener;
     @Override
     public void onCreate() {
         super.onCreate();
         mAuth=FirebaseAuth.getInstance();
-//        mAuth.useEmulator("10.0.2.2",9099);
                 FirebaseUser user=mAuth.getCurrentUser();
                 if(user!=null && user.isEmailVerified()){
-                    mAuth.removeAuthStateListener(authStateListener);
                     Intent intent= new Intent(getApplicationContext(),MainActivity.class);
                     intent.putExtra("ID",user.getUid());
                     intent.putExtra("EMAIL",user.getEmail());
@@ -34,4 +23,6 @@ public class Home extends Application {
                     startActivity(intent);
                 }
     }
+
+
 }
